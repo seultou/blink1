bb49d7ee3a503eb3938ad4fb11250aaba2e2fc77
Seultou <c.delapasse@gmail.com>
2022-08-05 22:33:16 +0200

    add speed option to fading

--- lib/color.py
+++ lib/color.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 from typing import final
+
 
 
 class Color:
@@ -6,8 +8,14 @@ class Color:
     GREEN: final = (1000, 0, 255, 0)
     __value: tuple = None
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.__value = getattr(self, name.upper())
 
     def settings(self) -> tuple:
         return self.__value
+
+    def speed(self, speed: int = 300) -> Color:
+        list(self.__value)[0] = speed
+        self.__value = tuple(self.__value)
+
+        return self
